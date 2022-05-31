d342cc8363a988dda1a44e70ad0715d916882a87
Ayushi0516 <ayushigupta0516@gmail.com>
2022-05-31 17:16:38 +0530

    chakra navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Box ,Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Box  display="flex"  border="1px solid blue" w='100%' p={4} color='black'>
+     <Box  fontWeight="bold">
+       <p>LOGO</p>
+     </Box>
+     
+     <Box w="30%" display="flex" justifyContent="space-between" ml={10}>
+       <p>Inspiration</p>
+       <p>Find Work</p>
+       <p>Learn Design</p>
+       <p>Hire Designers</p>
+       </Box>
+     <Box  w="20%" display="flex" ml={750} justifyContent="space-evenly">
+       <Button backgroundColor="white" >Sign in</Button>
+       <Button backgroundColor="red" color="white">Sign up</Button>
+     </Box>
+</Box>
     </div>
   );
 }
